8efe2572a16dccb3ae4d3ad2029b1330c1574097
Irving Luna <Irving Luna>
2017-05-04 06:08:25 -0500

    first commit

---
+++ App_Code/RectangleCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://servicios.web.rectangleCalculator/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+
+public class RectangleCalculator : System.Web.Services.WebService
+{
+    public RectangleCalculator () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public double area(double h, double b) {
+        return h*b;
+    }
+    
+}
